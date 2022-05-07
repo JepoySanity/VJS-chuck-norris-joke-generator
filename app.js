855c220bab4f88e9af1959fc5e024a8c87fa1290
Jepoy Factoran <factoran20@gmail.com>
2022-05-07 20:43:59 +0800

    append to jokes if xhr response type is equal to success

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ function getJokes(e) {
   xhr.open("GET", `https://api.icndb.com/jokes/random/${numJokes}`, true);
   xhr.onload = function () {
     if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      //check for api response type
+      if (response.type === "success") {
+        let output = "";
+        response.value.forEach(function (joke) {
+          output += `
+          <div class="card mb-2">
+            <div class="card-header">
+              <h6>JOKE #${joke.id}</h6>
+            </div>
+            <div class="card-body">
+              <h6>
+                ${joke.joke}
+              </h6>
+            </div>
+          </div>
+          `;
+        });
+        document.getElementById("jokes").innerHTML = output;
+        console.log(output);
+      }
     }
   };
   xhr.send();
