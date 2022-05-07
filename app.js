20ffb5d31cd8525963ad9dd9730bbbf69baf23df
Jepoy Factoran <factoran20@gmail.com>
2022-05-07 20:33:42 +0800

    prepare xhr for api request

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 document.getElementById("get-jokes").addEventListener("click", getJokes);
+
+function getJokes(e) {
+  e.preventDefault();
+  const numJokes = document.getElementById("num-jokes").value;
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", `https://api.icndb.com/jokes/random/${numJokes}`, true);
+  xhr.onload = function () {
+    if (this.status === 200) {
+    }
+  };
+  xhr.send();
+}
